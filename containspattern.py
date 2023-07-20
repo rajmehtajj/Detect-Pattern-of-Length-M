ec1d1c3b8806dc8d15542266cf391a76401749d7
Raj Mehta <raj.mehtajj@gmail.com>
2023-07-19 23:42:40 -0600

    working on edge cases

--- containspattern.py
+++ containspattern.py
@@ -2,15 +2,24 @@ from typing import List
 
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        matches_found = 0
         for i in range(len(arr) - m):
-            pattern_match = arr[i:(i+m+1)]
-            print("looking for pattern:", pattern_match)
+            pattern = arr[i:(i+m)]
+            print("looking for pattern:", pattern)
             for j in range(i+m, len(arr),m):
-                candidate = arr[j:(j+m+1)]
+                candidate = arr[j:(j+m)]
                 print("\tcandidate", candidate)
 
+                if (candidate == pattern):
+                    matches_found += 1
+                    print("match found:", matches_found)
+                    if matches_found == k:
+                        return True
+                else:
+                    break
 
-        return True
+
+        return False
 
 
 #arr = [1,2,4,4,4,4]
@@ -18,7 +27,8 @@ class Solution:
 #k = 3
 
 arr = [1,2,1,2,1,3]
-m = 2
-k = 3
+#arr = [10,20,30,40,50,30,40,80,90]
+m = 3
+k = 2
 s = Solution()
 print(s.containsPattern(arr, m, k))
